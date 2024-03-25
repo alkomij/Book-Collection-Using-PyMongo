23a325c40c16c0f2dab8f51a3beab811a3aad8f4
ImanYeganeh <imanyegane30@gmail.com>
2024-03-25 18:44:18 -0400

    make chaneges for the transactions

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Version: 1.0
 # Date: Mar 22, 2024
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
 from dotenv import load_dotenv
@@ -33,17 +33,23 @@ app.register_blueprint(book_routes, url_prefix='/books')
 app.register_blueprint(user_routes, url_prefix='/users')
 app.register_blueprint(transaction_routes, url_prefix='/transactions')
 
+# Define a route for the root URL "/"
+@app.route('/')
+def home():
+    return jsonify(message="Welcome to our Bookstore/Library Management System!"), 200
+
 # Catch unhandled requests (404 Not Found)
 @app.errorhandler(404)
 def not_found(error):
-    return {"error": "Not Found"}, 404
+    return jsonify(error="Not Found"), 404
 
 # Global error handler (500 Internal Server Error)
 @app.errorhandler(500)
 def server_error(error):
-    return {"error": "Internal Server Error"}, 500
+    return jsonify(error="Internal Server Error"), 500
 
 # Start the server
 if __name__ == '__main__':
-    PORT = os.getenv('PORT', 3000)
+    # Use PORT environment variable if it exists, otherwise default to 3000
+    PORT = int(os.getenv('PORT', 3000))
     app.run(debug=True, port=PORT)
